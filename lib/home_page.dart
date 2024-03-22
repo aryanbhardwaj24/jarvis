6bc4098d270aa98a67cd151e0169e22fef13b8ff
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-23 02:41:10 +0530

    add the custom virtual assistant image to the home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -26,6 +26,36 @@ class _HomePageState extends State<HomePage> {
           size: 25,
         ),
       ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  margin: const EdgeInsets.only(
+                    top: 4,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Pallete.assistantCircleColor,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              Container(
+                height: 123,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/virtualAssistant.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
