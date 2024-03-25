5ae98eda1ea09097f7900330604dd3e90887df8f
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-03-25 07:21:14 +0530

    add animations to the widgets in the home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:jarvis/feature_box.dart';
@@ -20,6 +21,8 @@ class _HomePageState extends State<HomePage> {
   FlutterTts flutterTts = FlutterTts();
   String? generatedContent;
   String? generatedImageUrl;
+  int start = 200;
+  int delay = 200;
 
   @override
   void initState() {
@@ -69,11 +72,13 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Jarvis',
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
+        title: BounceInDown(
+          child: const Text(
+            'Jarvis',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
         centerTitle: true,
@@ -86,61 +91,65 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             //Virtual Assistant Picture
-            Stack(
-              children: [
-                Center(
-                  child: Container(
-                    height: 120,
-                    width: 120,
-                    margin: const EdgeInsets.only(
-                      top: 4,
+            ZoomIn(
+              child: Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 120,
+                      width: 120,
+                      margin: const EdgeInsets.only(
+                        top: 4,
+                      ),
+                      decoration: const BoxDecoration(
+                        color: Pallete.assistantCircleColor,
+                        shape: BoxShape.circle,
+                      ),
                     ),
+                  ),
+                  Container(
+                    height: 123,
                     decoration: const BoxDecoration(
-                      color: Pallete.assistantCircleColor,
                       shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/virtualAssistant.png'),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  height: 123,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/virtualAssistant.png'),
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
             //Chat Bubble
-            Visibility(
-              visible: generatedImageUrl == null,
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
-                  top: 30,
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Pallete.borderColor,
+            FadeInRight(
+              child: Visibility(
+                visible: generatedImageUrl == null,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
                   ),
-                  borderRadius: BorderRadius.circular(20).copyWith(
-                    topLeft: Radius.zero,
+                  margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+                    top: 30,
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Text(
-                    generatedContent == null
-                        ? 'Hello, what task can I do for you?'
-                        : generatedContent!,
-                    style: TextStyle(
-                      fontFamily: 'Cera Pro',
-                      fontSize: generatedContent == null ? 25 : 18,
-                      color: Pallete.mainFontColor,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Pallete.borderColor,
+                    ),
+                    borderRadius: BorderRadius.circular(20).copyWith(
+                      topLeft: Radius.zero,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Text(
+                      generatedContent == null
+                          ? 'Hello, what task can I do for you?'
+                          : generatedContent!,
+                      style: TextStyle(
+                        fontFamily: 'Cera Pro',
+                        fontSize: generatedContent == null ? 25 : 18,
+                        color: Pallete.mainFontColor,
+                      ),
                     ),
                   ),
                 ),
@@ -155,22 +164,24 @@ class _HomePageState extends State<HomePage> {
                     child: Image.network(generatedImageUrl!)),
               ),
             //Descriptive Text
-            Visibility(
-              visible: generatedContent == null && generatedImageUrl == null,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                alignment: Alignment.centerLeft,
-                margin: const EdgeInsets.only(
-                  top: 10,
-                  left: 22,
-                ),
-                child: const Text(
-                  'Here are a few features',
-                  style: TextStyle(
-                    fontFamily: 'Cera Pro',
-                    color: Pallete.mainFontColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+            SlideInLeft(
+              child: Visibility(
+                visible: generatedContent == null && generatedImageUrl == null,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    left: 22,
+                  ),
+                  child: const Text(
+                    'Here are a few features',
+                    style: TextStyle(
+                      fontFamily: 'Cera Pro',
+                      color: Pallete.mainFontColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
@@ -178,25 +189,40 @@ class _HomePageState extends State<HomePage> {
             //Features List
             Visibility(
               visible: generatedContent == null && generatedImageUrl == null,
-              child: const Column(
+              child: Column(
                 children: [
-                  FeatureBox(
-                    boxColor: Pallete.firstSuggestionBoxColor,
-                    headerText: 'ChatGPT',
-                    descriptionText:
-                        'A smarter way to stay organized and informed with ChatGPT',
+                  SlideInLeft(
+                    delay: Duration(
+                      milliseconds: start,
+                    ),
+                    child: const FeatureBox(
+                      boxColor: Pallete.firstSuggestionBoxColor,
+                      headerText: 'ChatGPT',
+                      descriptionText:
+                          'A smarter way to stay organized and informed with ChatGPT',
+                    ),
                   ),
-                  FeatureBox(
-                    boxColor: Pallete.secondSuggestionBoxColor,
-                    headerText: 'Dall-E',
-                    descriptionText:
-                        'Get inspired and stay creative with your personal assistant powered by Dall-E',
+                  SlideInLeft(
+                    delay: Duration(
+                      milliseconds: start + delay,
+                    ),
+                    child: const FeatureBox(
+                      boxColor: Pallete.secondSuggestionBoxColor,
+                      headerText: 'Dall-E',
+                      descriptionText:
+                          'Get inspired and stay creative with your personal assistant powered by Dall-E',
+                    ),
                   ),
-                  FeatureBox(
-                    boxColor: Pallete.thirdSuggestionBoxColor,
-                    headerText: 'Smart Voice Assistant',
-                    descriptionText:
-                        'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+                  SlideInLeft(
+                    delay: Duration(
+                      milliseconds: start + 2 * delay,
+                    ),
+                    child: const FeatureBox(
+                      boxColor: Pallete.thirdSuggestionBoxColor,
+                      headerText: 'Smart Voice Assistant',
+                      descriptionText:
+                          'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+                    ),
                   ),
                 ],
               ),
@@ -204,32 +230,38 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          if (await speechToText.hasPermission && speechToText.isNotListening) {
-            await startListening();
-          } else if (speechToText.isListening) {
-            final speech = await openAIService.isArtPromptAPI(lastWords);
+      floatingActionButton: ZoomIn(
+        delay: Duration(
+          milliseconds: start + 3 * delay,
+        ),
+        child: FloatingActionButton(
+          onPressed: () async {
+            if (await speechToText.hasPermission &&
+                speechToText.isNotListening) {
+              await startListening();
+            } else if (speechToText.isListening) {
+              final speech = await openAIService.isArtPromptAPI(lastWords);
 
-            if (speech.contains('https')) {
-              generatedImageUrl = speech;
-              generatedContent = null;
-              setState(() {});
+              if (speech.contains('https')) {
+                generatedImageUrl = speech;
+                generatedContent = null;
+                setState(() {});
+              } else {
+                generatedImageUrl = null;
+                generatedContent = speech;
+                setState(() {});
+                await systemSpeak(speech);
+              }
+
+              await stopListening();
             } else {
-              generatedImageUrl = null;
-              generatedContent = speech;
-              setState(() {});
-              await systemSpeak(speech);
+              initSpeechToText();
             }
-
-            await stopListening();
-          } else {
-            initSpeechToText();
-          }
-        },
-        backgroundColor: Pallete.firstSuggestionBoxColor,
-        child: Icon(
-          speechToText.isListening ? Icons.stop_circle : Icons.mic,
+          },
+          backgroundColor: Pallete.firstSuggestionBoxColor,
+          child: Icon(
+            speechToText.isListening ? Icons.stop_circle : Icons.mic,
+          ),
         ),
       ),
     );
